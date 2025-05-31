b688b22c1bf9e713a8252ddb8893f54ba12cccaf
Miguel Senna <miguelsenna10@hotmail.com>
2025-05-31 15:42:46 -0300

    add npm start

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Pergunta = require("./database/Pergunta");
 const Resposta = require("./database/Resposta");
 const { where } = require("sequelize");
 //Database
+/*
 connection
     .authenticate()
     .then(() => {
@@ -14,15 +15,16 @@ connection
     .catch((msgErro) => {
         console.log(msgErro);
     })
-
+*/
 // Estou dizendo para o Express usar o EJS como View engine
-app.set('view engine' , 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 // Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Rotas
+/*
 app.get("/",(req, res) => {
     Pergunta.findAll({ raw: true, order:[
         ['id','DESC'] // ASC = Crescente || DESC = Decrescente
@@ -33,57 +35,61 @@ app.get("/",(req, res) => {
     }); 
 });
 
-app.get("/perguntar",(req, res) => {
-    res.render("perguntar");
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 });
 
-app.post("/salvarpergunta",(req, res) => {
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
 
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
-
-    Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
-    }).then(() => {
-        res.redirect("/");
-    })
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
-app.get("/pergunta/:id",(req ,res) => {
-    var id = req.params.id;
-    Pergunta.findOne({
-        where: {id, id}
-    }).then(pergunta => {
-        if(pergunta != undefined){ // Pergunta encontrada
+app.get("/pergunta/:id", (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id, id },
+  }).then((pergunta) => {
+    if (pergunta != undefined) {
+      // Pergunta encontrada
 
-            Resposta.findAll({
-                where: {perguntaId: pergunta.id},
-                order: [ 
-                    ['id','DESC']
-                ]
-            }).then(respostas => {
-                res.render("pergunta" ,{
-                    pergunta: pergunta,
-                    respostas: respostas
-                });
-            });
-
-        }else{ // Não encontrada
-            res.redirect("/");
-       }
-    });
-})
-
-app.post("/responder",(req, res) => {
-    var corpo = req.body.corpo;
-    var perguntaId = req.body.pergunta;
-    Resposta.create({
-        corpo: corpo,
-        perguntaId: perguntaId
-    }).then(() => {
-        res.redirect("/pergunta/"+perguntaId);
-    });
+      Resposta.findAll({
+        where: { perguntaId: pergunta.id },
+        order: [["id", "DESC"]],
+      }).then((respostas) => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas,
+        });
+      });
+    } else {
+      // Não encontrada
+      res.redirect("/");
+    }
+  });
 });
 
-app.listen(8080,() =>{console.log("App rodando!");});
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId,
+  }).then(() => {
+    res.redirect("/pergunta/" + perguntaId);
+  });
+});
+*/
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(8080, () => {
+  console.log("App rodando!");
+});
